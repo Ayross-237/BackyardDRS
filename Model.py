6ac16217c431ceed4c596dd34b382f4f087c3e58
Aaron D'Mello <aaron.r.dmello@gmail.com>
2025-12-19 23:47:05 +1000

    added dataclass for render method and continued implementation of model and video classes

--- Model.py
+++ Model.py
@@ -1,6 +1,19 @@
 import cv2 as cv
+import numpy as np
+from dataclasses import dataclass
+
+@dataclass
+class Render:
+    frontFrame: any
+    sideFrame: any
+    frontPoints: list[tuple[int]]
+    sidePoints: list[tuple[int]]
+    stumpPosition: int | None
 
 class Video:
+    """
+    A class to handle video processing and ball tracking.
+    """
     def __init__(self, filePath: str, ballColour: tuple[int]) -> None:
         """
         Initializes the Video object with the given parameters.
@@ -62,6 +75,13 @@ class Video:
         return self._curFrame.copy()
 
 
+    def getPoints(self):
+        """
+        TODO: DOCUMENT METHOD AND UPDDATE TYPE HINT
+        """
+        return self._points.copy()
+
+
     def incrementFrame(self) -> bool:
         """
         Advances to the next frame in the video and tracks the ball position if it has passed the 
@@ -83,4 +103,65 @@ class Video:
     def _trackBallInCurrentFrame(self) -> None:
         pass
 
+
+
+class Model:
+    """
+    A class to handle the ball tracking model.
+    """
+    def __init__(self, frontViddeo: Video, sideVideo: Video) -> None:
+        """
+        Initializes the Model object with the given video objects.
+
+        parameters:
+            frontVideo (Video): Video object for the front view.
+            sideVideo (Video): Video object for the side view.
+        """
+        self._frontVideo = frontViddeo
+        self._sideVideo = sideVideo
+        self._isLinked = False
+        self._stumpPosition = None
+    
+
+    def setStumpPosition(self, position: int) -> None: 
+        """
+        Sets the stump position from the view of the side video.
+
+        parameters:
+            position (int): The stump position.
+        """
+        self._stumpPosition = position
+
+
+    def linkVideos(self) -> bool:
+        """
+        Links the front and side video views for synchronized ball tracking.
+
+        returns:
+            bool: True if linking was successful, false otherwise.
+        """
+        self._isLinked = True
+    
+
+    def startTracking(self) -> None:
+        """
+        Starts the ball tracking process for both video views.
+        """
+        self._frontVideo.markFirstFrame()
+        self._sideVideo.markFirstFrame()
+    
+
+    def render(self) -> Render:
+        """
+        Renders the current frames and ball tracking points from both video views.
+
+        returns:
+            Render: A Render object containing the current frames and ball tracking points.
+        """
+        frontFrame = self._frontVideo.getCurrentFrame()
+        sideFrame = self._sideVideo.getCurrentFrame()
+        frontPoints = self._frontVideo.getPoints()
+        sidePoints = self._sideVideo.getPoints()
+        return Render(frontFrame, sideFrame, frontPoints, sidePoints, self._stumpPosition)
+        
     
